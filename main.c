8ea3c100c1601475a94825ae6239631babd31f29
Carlos Uliao <culiao@opencores.org>
2014-12-08 10:07:10 -0300

    Force input focus to current window / move windows to (0,0) during window switching

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-void raise_next_window (Display *dpy) {
+void raise_next_window (Display *dpy) 
+{
 	Window root, parent, *wins, *w;
 	XWindowAttributes attr;
 	unsigned int num_wins;
-	
-	/* Switch to the first viewable window we can find */
+
 	XQueryTree(dpy, DefaultRootWindow(dpy),	&root, &parent,	&wins, &num_wins);
-	for (w=wins; w < (wins+num_wins); w++) {
+
+	/* Switch to the first viewable window we can find */
+	for (w=wins; w < (wins+num_wins); w++) 
+	{
 		XGetWindowAttributes(dpy, *w, &attr);
-		if (attr.map_state == IsViewable) {
-			XResizeWindow(dpy, *w,
+		if (attr.map_state == IsViewable) 
+		{
+			XMoveResizeWindow(dpy, *w, 0, 0,
 				DisplayWidth(dpy, DefaultScreen(dpy)),
 				DisplayHeight(dpy, DefaultScreen(dpy)));
 			XRaiseWindow(dpy, *w);
+			XSetInputFocus(dpy, *w, 0, 0);
 			return;
 		}
 	}
 }
 
-int main() {
+int main() 
+{
 	Display *dpy;
 	Window root;
 	XEvent ev;
 
-	if (!(dpy = XOpenDisplay(0))) return 1;       /* "Cannot open display" */
-	root = DefaultRootWindow(dpy);	
-	XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Tab")), 
+	if (!(dpy = XOpenDisplay(0))) 
+		return 1;       /* "Cannot open display" */
+	
+	root = DefaultRootWindow(dpy);
+
+	XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Tab")),
 		Mod1Mask, root, True, GrabModeAsync, GrabModeAsync);
 
 	for (;;) {
 		XNextEvent(dpy, &ev);
 		if (ev.type == KeyPress) raise_next_window (dpy);
-	}		
+	}
 }
-
